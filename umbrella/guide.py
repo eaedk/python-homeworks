6129e93be67eed10635aea81e0a2b71d8b117c32
Emmanuel KOUPOH <emmanuelkoupoh@gmail.com>
2022-08-30 10:16:09 +0000

    guidance for learners

---
+++ umbrella/guide.py
@@ -0,0 +1,33 @@
+#UMBRELLA ASSIGNMENT : DO NOT MODIFY THIS FILE
+
+# You can import some libraries just bellow
+
+# Definition of the propasal solution function with comments to help you
+def proposal(weather=[]):
+    """
+    #
+    # @TODO: Write the doctring here: function's description + description of
+    # parameters and returned variables
+    """
+
+    #
+    # @TODO: Implement the logic of your solution here
+    #
+
+    # @GUIDE: You may follow the guide bellow
+
+
+    # The weather list may contain wrong number of weathers
+    # if a bad weather variable is passed: # len(weather)%3 is 0 if the length of weather is not a multiple of 3
+        # raise an error
+
+    # when the weather list is fine for processing
+        # iterate through the passed weather list
+        # loop: # big loop to iterate over the days: [day 1, day 2, etc ...] 
+            #set umbrella initial statement # the umbrella is in the bag when a day starts
+
+            # Analyze the 3 weather information of a day
+            # loop statement: # little loop to iterate over the parts of the day : [morning, afternoon, evening]
+                # Condition to take the umbrella out
+
+                # Condition put the umbrella in the bag
